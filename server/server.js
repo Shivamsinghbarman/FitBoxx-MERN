acb1c4b76722b49ce5757562d92ac388f5521e8c
Shivamsinghbarman <90323628+Shivamsinghbarman@users.noreply.github.com>
2024-10-17 21:09:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const routes = require("./routes");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
