79ec0d1ef5146f4799e934416cc5822dadcc8a6a
Shivamsinghbarman <90323628+Shivamsinghbarman@users.noreply.github.com>
2024-10-17 21:16:01 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -38,7 +38,6 @@ UserSchema.pre("save", async function (next) {
 
   next();
 });
-
 // custom method to compare and validate password for logging in
 UserSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
